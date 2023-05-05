8443794dd977ebf6d789e948a6e8c17e2595189c
safarilewis <lleewwii1@gmail.com>
2023-05-05 15:39:21 +0300

    updating require_auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,14 +5,19 @@ from typing import List, TypeVar
 
 
 class Auth:
-    '''Class that will contain auth methods'''
+    '''Class containing all auth methods'''
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns false """
-        return False
+    def require_auth(self, path: str, excluded_paths: List[str], strict_slashes=False) -> bool:
+        """Checks if the path provided requires authentication """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        for paths in excluded_paths:
+            if path == paths:
+                return False
+            return True
 
     def authorization_header(self, request=None) -> str:
-        """Returns None, flast request object"""
+        """Performs request validation to secure the API"""
         return request
 
     def current_user(self, request=None) -> TypeVar('User'):
