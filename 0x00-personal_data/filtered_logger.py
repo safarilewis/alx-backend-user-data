ed30f3fc4c859799480fe5cfcdb2700b162ac213
safarilewis <lleewwii1@gmail.com>
2023-05-26 01:24:37 +0300

    typing return value

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,9 +5,10 @@ import logging
 import re
 
 
-def filter_datum(fields: List[str], 
-                redaction: str, message: str, separator: str):
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
     '''Returns a log message obfuscated'''
     for item in fields:
-        message = re.sub(item+'=.*'+separator,item+'='+redaction+separator,message)
+        message = re.sub(item+'=.*'+separator, item +
+                         '='+redaction+separator, message)
     return message
