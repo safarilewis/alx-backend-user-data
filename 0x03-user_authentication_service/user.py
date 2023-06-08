ac6e19e05febec54a21849d71b265e1f052b0be4
safarilewis <lleewwii1@gmail.com>
2023-06-08 12:28:44 +0300

    implementing user class with mappings

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''User class mapped to sql'''
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    '''User Class'''
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
