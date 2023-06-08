ead204ce8c6319d35845ce19504b579d8c7ada09
safarilewis <lleewwii1@gmail.com>
2023-06-08 13:35:21 +0300

    changing arguments

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,11 +31,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         '''Adding user method'''
-        try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            new_user = None
-        return new_user
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
