5cb20dc8775f3a41cb8d820f3c645e86856f2972
safarilewis <lleewwii1@gmail.com>
2023-08-15 00:08:40 +0300

    fixing a silly typing error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -47,7 +47,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connector.connections.MySQLConnection:
+def get_db() -> mysql.connector.connection.MySQLConnection:
     '''Creates connection to db'''
     host = os.getenv(PERSONAL_DATA_DB_HOST)
     name = os.getenv(PERSONAL_DATA_DB_NAME)
